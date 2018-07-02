67f2f4035129df6198674d0100d286f39d44a324
Jigar-soni44 <40736739+Jigar-soni44@users.noreply.github.com>
2018-07-02 14:57:28 +0530

    small change - final

--- install-wp.sh
+++ install-wp.sh
@@ -19,7 +19,7 @@ then
 		echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/nginx-stable.list
         sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
         sudo apt-get update
-        sudo apt-get install nginx
+        sudo apt-get install nginx -y
 fi
 
 ###now we will ask user to enter domain name
